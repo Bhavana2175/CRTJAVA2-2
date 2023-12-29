408b31969e11010e37c779694100e8ca29a248a5
Bhavana2175 <155128683+Bhavana2175@users.noreply.github.com>
2023-12-29 14:07:58 +0530

    Add files via upload

---
+++ Nested.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class Nested
+{
+	public static void main(String[] args) 
+	{
+	Scanner s=new Scanner(System.in);
+	int a,b,c;
+	System.out.println("Enter 3 nos:");
+	a=s.nextInt();
+	b=s.nextInt();
+	c=s.nextInt();
+	if(a>b)
+	{
+		if(a>c)
+		{
+			System.out.println(a+" is biggest");
+		}
+		else
+		{
+			System.out.println(c+" is biggest");
+		}
+	}
+		else
+		if(b>c)
+		{
+			System.out.println(b+" is biggest");
+		}
+		else
+		{
+			System.out.println(c+" is biggest");
+		}
+	
+	}
+
+
+}
