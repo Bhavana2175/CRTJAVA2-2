3577e0a3ff092ced42c5347c194acd47c20ad39e
Bhavana2175 <155128683+Bhavana2175@users.noreply.github.com>
2023-12-30 14:29:38 +0530

    Add files via upload

---
+++ While2.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class While2
+
+{
+    
+public static void main(String[] args)
+    
+{
+    
+Scanner s=new Scanner(System.in);
+int i=1,n;
+System.out.println("Enter n nos");
+
+n=s.nextInt();
+while (i<=n)
+    
+{
+        
+System.out.print(i);
+        
+i=i+1;
+    
+}
+    
+}
+
+}
+
+
