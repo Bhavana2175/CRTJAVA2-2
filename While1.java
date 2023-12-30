5ea93a01fc6d29d6c9459a8cf62118f2ffe0ab8f
Bhavana2175 <155128683+Bhavana2175@users.noreply.github.com>
2023-12-30 14:26:57 +0530

    Add files via upload

---
+++ While1.java
@@ -0,0 +1,76 @@
+import java.util.*;
+
+public class While1
+
+{
+    
+public static void main(String[] args)
+    
+{
+    
+int i=10;
+    
+while (i>=1)
+    
+{
+        
+System.out.print(i+" ");
+        
+i=i-1;
+    
+}
+    
+}
+
+}
+import java.util.*;
+
+public class While1
+
+{
+    
+public static void main(String[] args)
+    
+{
+    
+int i=10;
+    
+while (i>=1)
+    
+{
+        
+System.out.print(i);
+        
+i=i-1;
+    
+}
+    
+}
+
+}
+
+import java.util.*;
+
+public class While1
+
+{
+    
+public static void main(String[] args)
+    
+{
+    
+int i=10;
+    
+while (i>=1)
+    
+{
+        
+System.out.println(i);
+        
+i=i-1;
+    
+}
+    
+}
+
+}
