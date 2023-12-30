46e8a6febab964b726c141d91145741557fa8a35
Bhavana2175 <155128683+Bhavana2175@users.noreply.github.com>
2023-12-30 15:13:32 +0530

    Add files via upload

---
+++ SumOfDigits.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class SumOfDigits
+{
+    
+public static void main(String[] args)
+    
+{
+    
+Scanner s=new Scanner(System.in);
+int n,r=0,d;
+System.out.println("Enter n value:");
+
+n=s.nextInt();
+while (n>0)
+    
+{
+       
+d=n%10;
+n=n/10:
+r=r+d; 
+System.out.print(r);
+        
+}
+    
+}
+
+}
+
+
