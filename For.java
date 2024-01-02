0df0440e014c1cdd0ac1edf8dfe570ebd1df60d8
Bhavana2175 <155128683+Bhavana2175@users.noreply.github.com>
2024-01-02 09:20:30 +0530

    Add files via upload

---
+++ For.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class For
+
+{
+	
+public static void main(String[] args) 
+{
+	    
+int n;
+	    
+for(n=1;n<=10;n++)
+	    
+{
+	    
+System.out.print(n+" ");
+	    
+}
+	
+}
+
+}
