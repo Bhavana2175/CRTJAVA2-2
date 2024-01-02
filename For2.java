754620ed4f1e131f5422b60d8d62950590d97ebd
Bhavana2175 <155128683+Bhavana2175@users.noreply.github.com>
2024-01-02 09:44:57 +0530

    Add files via upload

---
+++ For2.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main
+
+{
+	
+public static void main(String[] args) 
+{
+	    
+int n,i,r=0;
+	    
+Scanner s=new Scanner(System.in);
+	   
+	    
+n=s.nextInt();
+	    
+for(i=1;i<=n;i=i+1)
+	    
+{
+	    
+r=r+i;
+	    
+}
+	    
+	    
+System.out.println(r);
+	
+}
+
+
+}
