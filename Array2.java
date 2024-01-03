21b3f793c19cc2abbf33c48b7bc0f7364bc5da9d
Bhavana2175 <155128683+Bhavana2175@users.noreply.github.com>
2024-01-03 10:55:01 +0530

    Update Array2.java

--- Array2.java
+++ Array2.java
@@ -1,10 +1,10 @@
-class class Array2
+class Array2
 {
 	public static void main(String[] args) 
 	{
 		float a[]={34.5f,21.5f,55.9f};
 		int i;
-		for(i=0;i<=3,i++)
+		for(i=0;i<=3;i++)
 		{
 			System.out.println(a[i]);
 		}
