54aad9b2631ab1220946e124ad20e13f6d3be98d
Bhavana2175 <155128683+Bhavana2175@users.noreply.github.com>
2024-01-04 12:03:40 +0530

    Add files via upload

---
+++ Sample3.java
@@ -0,0 +1,15 @@
+class Sample3
+{
+	int a;
+	float b;
+	boolean c;
+	String d;
+	public static void main(String[] args)
+	{
+		Sample3 s1=new Sample3();
+		System.out.println(s1.a);
+		System.out.println(s1.b);
+		System.out.println(s1.c);
+	    System.out.println(s1.d);
+	}
+}
